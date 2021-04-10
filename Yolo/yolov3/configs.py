19386a8a08e780fbaa9948e40cb0d66abdbe2878
Arr0gant <albertoarjanlee@gmail.com>
2021-04-10 11:03:15 +0100

    Update configs.py

--- Yolo/yolov3/configs.py
+++ Yolo/yolov3/configs.py
@@ -10,7 +10,7 @@
 #================================================================
 
 # YOLO options
-YOLO_TYPE                   = "yolov3" # yolov4 or yolov3
+YOLO_TYPE                   = "yolov4" # yolov4 or yolov3
 YOLO_FRAMEWORK              = "tf" # "tf" or "trt"
 YOLO_V3_WEIGHTS             = "model_data/yolov3.weights"
 YOLO_V4_WEIGHTS             = "model_data/yolov4.weights"
@@ -24,7 +24,7 @@ YOLO_STRIDES                = [8, 16, 32]
 YOLO_IOU_LOSS_THRESH        = 0.5
 YOLO_ANCHOR_PER_SCALE       = 3
 YOLO_MAX_BBOX_PER_SCALE     = 100
-YOLO_INPUT_SIZE             = 320
+YOLO_INPUT_SIZE             = 416
 if YOLO_TYPE                == "yolov4":
     YOLO_ANCHORS            = [[[12,  16], [19,   36], [40,   28]],
                                [[36,  75], [76,   55], [72,  146]],
@@ -43,8 +43,8 @@ TRAIN_LOGDIR                = "log"
 TRAIN_CHECKPOINTS_FOLDER    = "checkpoints"
 TRAIN_MODEL_NAME            = f"{YOLO_TYPE}_custom"
 TRAIN_LOAD_IMAGES_TO_RAM    = True # With True faster training, but need more RAM
-TRAIN_BATCH_SIZE            = 2
-TRAIN_INPUT_SIZE            = 320
+TRAIN_BATCH_SIZE            = 32
+TRAIN_INPUT_SIZE            = 416
 TRAIN_DATA_AUG              = True
 TRAIN_TRANSFER              = True
 TRAIN_FROM_CHECKPOINT       = False # "checkpoints/yolov3_custom"
@@ -55,8 +55,8 @@ TRAIN_EPOCHS                = 60
 
 # TEST options
 TEST_ANNOT_PATH             = "./model_data/DatasetTest.txt"
-TEST_BATCH_SIZE             = 2
-TEST_INPUT_SIZE             = 320
+TEST_BATCH_SIZE             = 32
+TEST_INPUT_SIZE             = 416
 TEST_DATA_AUG               = False
 TEST_DECTECTED_IMAGE_PATH   = ""
 TEST_SCORE_THRESHOLD        = 0.3
