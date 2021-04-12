2f29ae2df2f75b20ea3a348a827025bc2c58c20e
Arr0gant <albertoarjanlee@gmail.com>
2021-04-12 15:23:04 +0100

    Update detection_custom.py

--- Yolo/detection_custom.py
+++ Yolo/detection_custom.py
@@ -20,8 +20,8 @@ image_path   = "D:/YoloV4/TensorFlow-2.x-YOLOv3-master/IMAGES/image2084.png"
 video_path   = "D:/YoloV4/TensorFlow-2.x-YOLOv3-master/primate_clip_744.mp4"
 
 yolo = Load_Yolo_model()
-detect_image(yolo, image_path, "D:/YoloV4/TensorFlow-2.x-YOLOv3-master/IMAGES/detect.jpg", input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
-#detect_video(yolo, video_path, 'D:/YoloV4/TensorFlow-2.x-YOLOv3-master/IMAGES/detected2.mp4', input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+#detect_image(yolo, image_path, "D:/YoloV4/TensorFlow-2.x-YOLOv3-master/IMAGES/detect.jpg", input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+detect_video(yolo, video_path, 'D:/YoloV4/TensorFlow-2.x-YOLOv3-master/IMAGES/detected2.mp4', input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
 #detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255, 0, 0))
 
 #detect_video_realtime_mp(video_path, "Output.mp4", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0), realtime=False)
